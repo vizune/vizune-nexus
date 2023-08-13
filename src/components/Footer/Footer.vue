178a32132638e2926022d154586477887e26e10a
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2023-08-14 00:39:26 +0100

    Footer style change

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -6,7 +6,7 @@ import Affiliates from './Affiliates.vue';
 </script>
 
 <template>
-    <footer id="Footer" class="bg-dark w-full grid grid-cols-1 md:grid-cols-2">
+    <footer id="Footer" class="bg-dark w-full grid grid-cols-1 md:grid-cols-2 border-b-4 border-toadstool">
         <Widget heading="Affiliates">
             <Affiliates />
         </Widget>
