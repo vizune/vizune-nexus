ded09462f7e7505db39fef1a7eece974a8cb2551
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2023-11-26 20:16:21 +0000

    Performance amends

--- src/components/Header/Logo.vue
+++ src/components/Header/Logo.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="flex justify-center p-4">
-        <img src="/logo.png" alt="Vizune" />
+        <img src="/logo.png" alt="Vizune" width="200" height="50" />
     </div>
 </template>
 
